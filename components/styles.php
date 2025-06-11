d8a5f41538c80a221a48e0edf60a75ef2995670b
Joseph-67 <108576485+Joseph-67@users.noreply.github.com>
2025-06-11 12:54:29 +0100

    summary

--- components/styles.php
+++ components/styles.php
@@ -1,20 +1,20 @@
  <!-- ** Plugins Needed for the Project ** -->
   <!-- Bootstrap -->
-  <link rel="stylesheet" href="../assets/plugins/bootstrap/bootstrap.min.css">
+  <link rel="stylesheet" href="../../assets/plugins/bootstrap/bootstrap.min.css">
   <!-- slick slider -->
-  <link rel="stylesheet" href="../assets/plugins/slick/slick.css">
+  <link rel="stylesheet" href="../../assets/plugins/slick/slick.css">
   <!-- themefy-icon -->
-  <link rel="stylesheet" href="../assets/plugins/themify-icons/themify-icons.css">
+  <link rel="stylesheet" href="../../assets/plugins/themify-icons/themify-icons.css">
   <!-- animation css -->
-  <link rel="stylesheet" href="../assets/plugins/animate/animate.css">
+  <link rel="stylesheet" href="../../assets/plugins/animate/animate.css">
   <!-- aos -->
-  <link rel="stylesheet" href="../assets/plugins/aos/aos.css">
+  <link rel="stylesheet" href="../../assets/plugins/aos/aos.css">
   <!-- venobox popup -->
-  <link rel="stylesheet" href="../assets/plugins/venobox/venobox.css">
+  <link rel="stylesheet" href="../../assets/plugins/venobox/venobox.css">
 
   <!-- Main Stylesheet -->
-  <link href="../assets/css/style.css" rel="stylesheet">
-  
+  <link href="../../assets/css/style.css" rel="stylesheet">
+
   <!--Favicon-->
   <link rel="shortcut icon" href="https://themewagon.github.io/educenter/images/favicon.ico" type="image/x-icon">
   <link rel="icon" href="https://themewagon.github.io/educenter/images/favicon.ico" type="image/x-icon">
