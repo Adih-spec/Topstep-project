994567fe9d3e304f99429f7ef68914373bbedef6
Jamani-16 <mprosper385@gmail.com>
2025-06-24 20:23:27 +0100

    update on program.php

---
+++ views/web/programs.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>Programs  topstepsacademy</title>
+  <?php require_once '../../components/styles.php'; ?>
+
+
+  
+  <!-- footer -->
+<?php include_once '../../components/footer.php'; ?>
+<?php include_once '../../components/scripts.php'; ?>
