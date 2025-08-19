32fc5232d9390b20d226ecdfa6aa5d66f48a5ffe
burnah1997 <olawaleolukoya7@gmail.com>
2025-08-19 13:57:47 +0100

    summmary

---
+++ TPS-PR/resources/views/components/pages/admission.blade.php
@@ -0,0 +1,89 @@
+@extends('components.layouts.app')
+@section('PageTitle', 'Home')
+@section('pageContent')
+ <!-- page title -->
+<section class="page-title-section overlay" data-background="images/backgrounds/page-title.jpg">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8">
+        <ul class="list-inline custom-breadcrumb">
+          <li class="list-inline-item"><a class="h2 text-primary font-secondary" href="Admission Process.html">Admission Process</a></li>
+          <li class="list-inline-item text-white h3 font-secondary nasted">Apply Now</li>
+        </ul>
+        <p class="text-lighten">How to apply to school.</p>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- /page title -->
+
+<!-- notice details -->
+<section class="section">
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <div class="d-flex">
+          <div class="text-center mr-4">
+            <div class="p-4 bg-primary text-white">
+                <span class="h2 d-block">30</span> AUGUST,2025
+            </div>
+          </div>
+          <!-- notice content -->
+          <div>
+            <h3 class="">Application </h3>
+            <ul class="list-styled mb-5">
+              <li>A non-refundable fee (contact the school for more information) for the 
+                application process should be paid to any of the provided school bank account.</li>
+              <li>Submit the bank teller, interbank transfer, online payment or ATM receipts 
+                and pickup an application form from the school Secretary. At this stage, 
+                a brief tour of the school can be agreed. It is always necessary to meet with the Admin Officer/Head of School as all admission enquiries require their involvement.</li>
+              <li>Complete the application form and submit to the Admin Officer together with:
+
+                    <ol> 1. Two passport- sized photographs (name written on the back)</ol>
+                    <ol>2. Photocopy of birth certificate or international passpor</ol> 
+                    <ol>3. Photocopy of last school report/Transfer Leaving Certificate for Basic 7, Basic 8 and SS2, and BECE & NECO results for SS1 applicants.</ol>
+                   <ol> 4. Photocopy of medical fitness form, health certificate/Immunization record card (this must be up-to-date at the point of admission).</ol>
+                   </li>
+                  </ul>
+          <h3 class="">Placement process </h3>
+            <ul class="list-styled mb-5">
+              <li>Following receipt of all documentations, applicants will be invited to the 
+                school for a placement test as appropriate for the required class. 
+                This test assesses an applicant’s learning potential based on the Universal Basic Education (UBE) system.</li>
+              <li>Applicants are offered places in school based on:
+                 <ol> 1. Results of educational assessments, which includes reports from previous school and placement test results</ol>
+                 <ol> 2. Siblings in the school</ol>
+                 <ol> 3.Date of submission of application </ol>
+                 <ol> 4.Medical grounds</ol></li>
+                 <li> Occasionally, we assess an applicant a class below the requested one, which depends on his/her performance in the
+                   placement test. However, we usually do not assess for a class above his/her current one, nor sustain any acceleration
+                    request made by his/her parent.</li>
+                <li>Any applicant relocating from a foreign country will be assessed by reference to his/her most recent school reports 
+                  alongside any other available information. </li>
+              </ul>
+        <h3 class="">Admission </h3>
+            <ul class="list-styled mb-5">
+              <li>After the placement test, the Head of School will discuss the results with the parent concerned and then decide on the 
+                allocation of the class. The school’s management decision is final.</li>
+              <li>An offer letter will be issued to you and must be signed by the parent(s) and returned to the school. The bill for the
+                 term’s fee will be issued separately.</li> 
+              </ul>
+        <h3 class="">Payment of fees </h3>
+          <ul class="list-styled mb-5">
+           <li>A learner new to Topsteps Academy will not be admitted until the term’s full fees has been paid.</li> 
+           <li>School fees for subsequent terms are normally issued alongside result for the term and the school newsletter.</li>
+           <li>The date by which school fees must be paid is clearly stated and we do respectfully request that parents note
+             and keep to these dates.</li>
+           <li>All payment evidence must clearly identify the children to whom they relate</li>
+           <li>It is the school policy not to accept any payment in cash.</li>
+           <li>If you are out of the country when fees are due, you must make arrangement for payment to be made.</li>
+              </ul>
+            <a href="notice-single.html#" class="btn btn-primary">Apply now</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- /notice details -->
+@endsection
