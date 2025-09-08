13ced6ee3adf9347d22d77c9c830b450bf8cef7f
Heisdominion <dominionenemona@gmail.com>
2025-09-08 18:32:47 +0200

    ..

---
+++ TPS-PR/app/Http/Controllers/DashboardController.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\students;
+// use App\Models\Course;
+// use App\Models\Program;
+// use App\Models\Enrollment;
+
+class DashboardController extends Controller{
+    public function index()
+{
+    $data['studentsCount'] = students::count();
+    // $coursesCount = Course::count();
+    // $programsCount = Program::count();
+    // $enrollmentsCount = Enrollment::count();
+
+    return view('components.student.dashboard', $data);
+}
+
+}
+
+
+
+?>
--- TPS-PR/routes/web.php
+++ TPS-PR/routes/web.php
@@ -4,21 +4,19 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PermissionController;
-use App\Http\Controllers\StudentController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/students', [StudentController::class, 'index'])->name('students.index');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/dashboard', function() {
+        view('dashboard');
+    })->name('dashboard');
 });
 
 Route::middleware(['auth'])->prefix('admin')->group(function () {
@@ -30,6 +28,7 @@ Route::middleware(['auth'])->prefix('admin')->group(function () {
         Route::put('/roles/{role}', 'update')->name('roles.update');
         Route::delete('/roles/{role}', 'destroy')->name('roles.destroy');
     });
+
     Route::controller(PermissionController::class)->group(function () {
         Route::get('/permissions', 'index')->name('permissions.index');
         Route::get('/permissions/create', 'create')->name('permissions.create');
@@ -37,9 +36,10 @@ Route::middleware(['auth'])->prefix('admin')->group(function () {
         Route::get('/permissions/{permission}/edit', 'edit')->name('permissions.edit');
         Route::put('/permissions/{permission}', 'update')->name('permissions.update');
         Route::delete('/permissions/{permission}', 'destroy')->name('permissions.destroy');
-    }
-    );
+    });
+
 });
 
 
 require __DIR__.'/auth.php';
+require __DIR__.'/student.php';
