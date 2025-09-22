7915b667a236a8b837a75a4f19c32067b7762b0b
delightmchl <delightmchl@gmail.com>
2025-09-22 08:25:16 -0700

    Update 2025_09_22_114459_create_subjects_table.php

--- TPS-PR/database/migrations/2025_09_22_114459_create_subjects_table.php
+++ TPS-PR/database/migrations/2025_09_22_114459_create_subjects_table.php
@@ -14,8 +14,9 @@ return new class extends Migration
     Schema::create('subjects', function (Blueprint $table) {
         $table->id();
         $table->string('name');
-        $table->timestamps();
-    });
+        $table->timestamps(); }
+    );
+   
 }
 
 
