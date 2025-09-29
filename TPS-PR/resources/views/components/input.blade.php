dbb007cc4309b3b22ea59ddd516242be58d4211b
delight <abolarinbabatunde88@gmail.com>
2025-09-29 12:00:25 +0100

    authentication

--- TPS-PR/resources/views/components/input.blade.php
+++ TPS-PR/resources/views/components/input.blade.php
@@ -1,3 +1,41 @@
-@props(['disabled' => false])
+@props([
+    'disabled' => false,
+    'icon' => 'ti ti-mail',
+    'type' => 'text',
+    'name' => '',
+    'placeholder' => '',
+    'value' => '',
+    'required' => false,
+    'autocomplete' => '',
+])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) !!}>
+<div class="position-relative mb-3">
+    @if($type != 'password' && $icon)
+        <span class="position-absolute top-50 start-0 translate-middle-y ps-3">
+            <i class="{{ $icon }}"></i>
+        </span>
+    @endif
+
+    <input
+        type="{{ $type }}"
+        name="{{ $name }}"
+        id="{{ $name }}"
+        placeholder="{{ $placeholder }}"
+        value="{{ old($name, $value) }}"
+        {{ $required ? 'required' : '' }}
+        {{ $autocomplete ? 'autocomplete='.$autocomplete : '' }}
+        {{ $disabled ? 'disabled' : '' }}
+        {{ $attributes->merge(['class' => 'form-control ' . ($icon && $type != 'password' ? 'ps-5 ' : '') . ($errors->has($name) ? 'is-invalid' : '')]) }}
+    >
+
+    @if($type === 'password')
+        <span class="toggle-password position-absolute top-50 end-0 translate-middle-y pe-3" style="cursor:pointer;">
+            <i class="ti ti-eye-off"></i>
+        </span>
+    @endif
+
+    @error($name)
+        <div class="invalid-feedback">{{ $message }}</div>
+    @enderror
+</div>
+<!-- End of Input Field Component -->
--- TPS-PR/resources/views/layouts/guest.blade.php
+++ TPS-PR/resources/views/layouts/guest.blade.php
@@ -1,27 +1,67 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="{{ asset('adminAssets/img/favicon.png') }}">
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{ asset('adminAssets/css/bootstrap.min.css') }}">
 
-        <!-- Styles -->
-        @livewireStyles
-    </head>
-    <body>
-        <div class="font-sans text-gray-900 antialiased">
-            {{ $slot }}
-        </div>
+    <!-- Icons -->
+    <link rel="stylesheet" href="{{ asset('adminAssets/plugins/icons/feather/feather.css') }}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/tabler-icons.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
-        @livewireScripts
-    </body>
+    <!-- Select2 CSS (optional) -->
+    <link rel="stylesheet" href="{{ asset('adminAssets/plugins/select2/css/select2.min.css')}}">
+
+    <!-- Animation & Extras (optional) -->
+    <link rel="stylesheet" href="{{ asset('MainAssets/plugins/animate/animate.css') }}">
+    <link rel="stylesheet" href="{{ asset('MainAssets/plugins/aos/aos.css') }}">
+    <link rel="stylesheet" href="{{ asset('MainAssets/plugins/venobox/venobox.css') }}">
+
+    <!-- Main CSS -->
+    <link rel="stylesheet" href="{{ asset('adminAssets/css/style.css')}}">
+
+    <!-- Vite for Laravel -->
+    @vite(['resources/js/app.js'])
+
+    @livewireStyles
+</head>
+<body class="account-page">
+    <div class="main-wrapper">
+        {{ $slot }}
+    </div>
+
+    @livewireScripts
+
+
+	<!-- jQuery -->
+	<script src="https://preskool.dreamstechnologies.com/html/template/assets/js/jquery-3.7.1.min.js" type="5ca06f06e343dd8e19b209a1-text/javascript"></script>
+
+	<!-- Bootstrap Core JS -->
+	<script src="https://preskool.dreamstechnologies.com/html/template/assets/js/bootstrap.bundle.min.js" type="5ca06f06e343dd8e19b209a1-text/javascript"></script>
+
+	<!-- Feather Icon JS -->
+	<script src="https://preskool.dreamstechnologies.com/html/template/assets/js/feather.min.js" type="5ca06f06e343dd8e19b209a1-text/javascript"></script>
+
+	<!-- Slimscroll JS -->
+	<script src="https://preskool.dreamstechnologies.com/html/template/assets/js/jquery.slimscroll.min.js" type="5ca06f06e343dd8e19b209a1-text/javascript"></script>
+
+	<!-- Select2 JS -->
+	<script src="https://preskool.dreamstechnologies.com/html/template/assets/plugins/select2/js/select2.min.js" type="5ca06f06e343dd8e19b209a1-text/javascript"></script>
+
+	<!-- Custom JS -->
+	<script src="https://preskool.dreamstechnologies.com/html/template/assets/js/script.js" type="5ca06f06e343dd8e19b209a1-text/javascript"></script>
+
+    <script src="https://preskool.dreamstechnologies.com/cdn-cgi/scripts/7d0fa10a/cloudflare-static/rocket-loader.min.js" data-cf-settings="5ca06f06e343dd8e19b209a1-|49" defer></script>
+
+    @stack('scripts')
+</body>
 </html>
