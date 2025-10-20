9efc30a2493cf4c6f95076270e6bb2a963cbaac3
Adih-spec <adihekpueezekiel@gmail.com>
2025-10-20 15:06:23 -0700

    summary

--- TPS-PR/database/migrations/2025_09_12_221925_create_students_table.php
+++ TPS-PR/database/migrations/2025_09_12_221925_create_students_table.php
@@ -43,9 +43,9 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
-        Schema::dropIfExists('students');
-        $table->dropSoftDeletes();
-    }
+ public function down(): void
+{
+    Schema::dropIfExists('students');
+}
+
 };
